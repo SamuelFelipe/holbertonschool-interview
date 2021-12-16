4aefcb1e3af629c13b5ef90e1fa07bf39590b13b
SamuelFelipe <2354@holbertonschool.com>
2021-12-16 13:42:04 -0500

    Update - betty style

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -3,33 +3,33 @@
 
 /**
  * insert_node - insert a new node in a sorted linked lists
- * @head - linked lists head
- * @number - number to add
+ * @head: linked lists head
+ * @number: number to add
  * Return: the address of the new node, or NULL if it failed
  */
 
-listint_t *insert_node(listint_t **head, int number) {
+listint_t *insert_node(listint_t **head, int number)
+{
 
-    listint_t *new, *next;
+	listint_t *new, *next;
 
-    next = *head;
+	next = *head;
 
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-      // failed to alocate needed memory
-      return NULL;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 
-    new->n = number;
-    new->next = NULL;
+	new->n = number;
+	new->next = NULL;
 
-    if (next == NULL)
-      *head = new;
+	if (next == NULL)
+		*head = new;
 
-    while (next->next->n < number)
-      next = next->next;
+	while (next->next->n < number)
+		next = next->next;
 
-    new->next = next->next;
-    next->next = new;
+	new->next = next->next;
+	next->next = new;
 
-    return new;
+	return (new);
 }
